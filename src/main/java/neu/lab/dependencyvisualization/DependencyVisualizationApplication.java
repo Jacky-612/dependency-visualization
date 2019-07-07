692140b274c20aa72be949ecb32e3a1f01ba7490
AsterSea <858256500@qq.com>
2019-07-07 19:50:06 +0800

    Initial commit

---
+++ src/main/java/neu/lab/dependencyvisualization/DependencyVisualizationApplication.java
@@ -0,0 +1,14 @@
+package neu.lab.dependencyvisualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class DependencyVisualizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyVisualizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/neu/lab/dependencyvisualization/DependencyVisualizationApplicationTests.java
@@ -0,0 +1,16 @@
+package neu.lab.dependencyvisualization;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DependencyVisualizationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
